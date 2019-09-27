b1202571c206652b1278922c0050b107895aeacd
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2019-09-27 18:54:59 +0300

    Add api to app

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 """Main file"""
 from flask import Flask
 from flask_migrate import Migrate
-from src.views import views
 
-from src.models import db
 from src.helpers import bcrypt
+from src.models import db
+from src.recycles import recycles
+from src.views import views
 
 
 def create_app(config_object):
@@ -13,6 +14,7 @@ def create_app(config_object):
     app.config.from_object(config_object)
 
     app.register_blueprint(views)
+    app.register_blueprint(recycles)
 
     db.init_app(app)
     Migrate(app, db)
--- src/recycles.py
+++ src/recycles.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource
+
+from src.models import Recycle
 
 recycles = Blueprint('recycles', __name__)
 api = Api(recycles)
@@ -9,6 +11,8 @@ class RecyclesList(Resource):
     """List of recycle units"""
     def get(self):
         """Get list of recycle units"""
-
     def post(self):
         """Create new recycle unit"""
+
+
+api.add_resource(RecyclesList, '/recycles')
